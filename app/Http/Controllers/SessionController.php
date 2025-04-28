06174078c8e28bf99ed563e1fde6999c3426ca2c
mohmedShafie <mohamedhassanelshafay@gmail.com>
2025-04-28 16:26:48 +0300

    fix bugs

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -14,6 +14,7 @@ class SessionController extends BaseController
 
         $sessions = Session::where('customer_id', config('customer.id'))
             ->with('messages')
+            ->orderBy('id', 'desc')
             ->offset($offset)
             ->limit($limit)
             ->get();
