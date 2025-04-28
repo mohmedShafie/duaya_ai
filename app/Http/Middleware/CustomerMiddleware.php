92b41cb0470baad24c4d1d9070c435ba43995112
mohmedShafie <mohamedhassanelshafay@gmail.com>
2025-04-28 15:22:41 +0300

    fix bugs

---
+++ app/Http/Middleware/CustomerMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\CPU\CustomerAuthHelper;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        if (Auth::guard('sanctum')->check()) {
+            CustomerAuthHelper::setConfig();
+
+            return $next($request);
+        }
+    }
+}
